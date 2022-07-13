e7ee23960dc055a0f245ddd8c33f2aaeb5956980
LiuBenny0206 <109209156+LiuBenny0206@users.noreply.github.com>
2022-07-13 21:03:05 +0800

    PC python

---
+++ Number.py
@@ -0,0 +1,12 @@
+from math import *
+print(2.12344156)
+
+print(10 % 2)
+my_num= 23
+print(str(my_num) + " not good ")
+print(abs(my_num))
+print(pow(2, 52))
+print(max(60, 40))
+print(min(-50, -60))
+print(round(3.5))
+print(sqrt(36))
